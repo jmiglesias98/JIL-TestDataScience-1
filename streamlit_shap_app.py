bf125fefa89044b26f2ce888f9ed36e6683fbce8
Juan Miguel Iglesias Labraca <iglesiaslabraca98@gmail.com>
2025-10-16 23:39:38 +0200

    Update streamlit_shap_app.py

--- streamlit_shap_app.py
+++ streamlit_shap_app.py
@@ -6,6 +6,7 @@ import plotly.graph_objects as go
 from io import BytesIO
 import requests
 import streamlit as st
+import dill
 
 st.set_page_config(layout="wide", page_title="What-if SHAP Explorer")
 
@@ -24,50 +25,7 @@ def fetch_url(url):
 @st.cache_data
 def load_df_from_bytes(bts):
     return pd.read_csv(BytesIO(bts))
-
-class DataCleaner(BaseEstimator, TransformerMixin):
-    def __init__(self):
-        self.categorical_cols = {
-            "job": ["admin.", "unknown", "unemployed", "management", "housemaid",
-                    "entrepreneur", "student", "blue-collar", "self-employed",
-                    "retired", "technician", "services"],
-            "marital": ["married","divorced","single"],
-            "education": ["unknown","secondary","primary","tertiary"],
-            "default": ["yes","no"],
-            "housing": ["yes","no"],
-            "loan": ["yes","no"],
-            "contact": ["unknown","telephone","cellular"],
-            "month": ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"],
-            "poutcome": ["unknown","other","failure","success"]
-        }
-
-class PreprocesadorDinamico(BaseEstimator, TransformerMixin):
-    def __init__(self, cols_to_drop_after_ohe=None):
-        self.cols_to_drop_after_ohe = cols_to_drop_after_ohe
-        self.ct = None
-
-    def fit(self, X, y=None):
-        num_cols = X.select_dtypes(include=["int64", "float64"]).columns.tolist()
-        cat_cols = X.select_dtypes(include=["object"]).columns.tolist()
-
-        num_transformer = Pipeline(steps=[
-            ("imputer", SimpleImputer(strategy="median")),
-            ("scaler", StandardScaler())
-        ])
-
-        cat_transformer = Pipeline(steps=[
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            ("ohe", OneHotEncoder(drop="first", handle_unknown="ignore", sparse_output=False))
-        ])
-
-        self.ct = ColumnTransformer(transformers=[
-            ("num", num_transformer, num_cols),
-            ("cat", cat_transformer, cat_cols)
-        ])
-
-        self.ct.fit(X)
-        return self
-        
+    
 @st.cache_data
 def load_model_from_bytes(bts):
     return pickle.loads(bts)
